5a3932a05e197d0aea2fc663e98e59998200d37d
valdenio458 <valdeniomartinho@gmail.com>
2022-10-06 15:08:20 -0300

    Array

--- cookbook.rb
+++ cookbook.rb
@@ -1,7 +1,11 @@
 puts "Bem-vindo ao Cookbook, sua rede social de receitas"
 
+receitas = []
+
 puts "Digite o nome da receita:"
 nome = gets.chomp()
+
+receitas << nome 
 
 # puts "Receita " + nome + " cadastrada com sucesso!"
 puts "Receita #{nome} cadastrada com sucesso!"
