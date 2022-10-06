af1a287e10fc514e5d71daaf081fc57ae034f7ce
valdenio458 <valdeniomartinho@gmail.com>
2022-10-06 15:37:58 -0300

    Estruturas de repetição

--- cookbook.rb
+++ cookbook.rb
@@ -2,10 +2,27 @@ puts "Bem-vindo ao Cookbook, sua rede social de receitas"
 
 receitas = []
 
-puts "Digite o nome da receita:"
-nome = gets.chomp()
 
-receitas << nome 
+while(true) do
+  puts "Digite o nome da receita:"
+  nome = gets.chomp()
 
-# puts "Receita " + nome + " cadastrada com sucesso!"
-puts "Receita #{nome} cadastrada com sucesso!"
+  receitas << nome 
+
+  puts
+  # puts "Receita " + nome + " cadastrada com sucesso!"
+  puts "Receita #{nome} cadastrada com sucesso!"
+  puts
+  puts "========== Receitas ========== "
+  # for em ruby:
+  # for receita in receitas
+  #   puts receita
+  # end
+
+  # for each em ruby:
+  # receitas.each do |receita|
+  #   puts receita
+  # end
+  puts receitas
+  puts
+end
